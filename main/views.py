bff010a37cb3c4ff361173cb78943cd49766e080
Greendeer07 <abanolja@gmail.com>
2026-01-29 18:26:58 +0900

    template, static 추가

--- main/views.py
+++ main/views.py
@@ -1,7 +1,2 @@
 from django.shortcuts import render
 from articles.models import Article
-
-def home(request):
-    # 최신 기사 5개를 가져옴
-    latest_articles = Article.objects.all().order_by('-created_at')[:5]
-    return render(request, 'main/home.html', {'articles': latest_articles})
