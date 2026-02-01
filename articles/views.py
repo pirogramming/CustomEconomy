d4fa15b5fce171d0cb6449a526cbd7185198ec2a
lilyyang0077 <lilyyang0077@test.com>
2026-02-01 13:39:53 +0900

    기사 목록 뜨도록 구현

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from .models import Article
 
-def articleList_view(request) :
-	return render(request, 'articleList.html')
+
+def articleList_view(request):
+    articles = Article.objects.select_related('category').order_by('-published_at')
+    
+    context = {
+        'articles': articles
+    }
+    
+    return render(request, 'articleList.html', context)
