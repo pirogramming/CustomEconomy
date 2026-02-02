9205af3d68b321361a8ca114e4737151fb2ba07b
xihxxn <sihyun8870@naver.com>
2026-02-02 13:07:30 +0900

    is_popular 추가

--- articles/models.py
+++ articles/models.py
@@ -17,6 +17,7 @@ class Article(models.Model):
     source = models.CharField(max_length=50)
     published_at = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
+    is_popular = models.BooleanField(default=False)
 
     # User와의 Bookmark 관계를 ManyToMany로 정의할 수 있습니다.
     bookmarked_by = models.ManyToManyField(User, through='UserBookmark', related_name='bookmarked_articles')
