f7d3bf021865414f813ed1c5e709bbdc37af9441
haeuniea <haeunhaa@gmail.com>
2026-02-02 13:13:07 +0900

    퀴즈 모델, load_to_db.py 수정

--- quizzes/models.py
+++ quizzes/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-from articles.models import Article
+from articles.models import Article, Category
 from accounts.models import User
 
 class Quiz(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='quizzes')
     level = models.IntegerField()
     question = models.TextField()
+    explanation = models.TextField(help_text="정답 해설")
     type = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -14,16 +16,11 @@ class QuizChoice(models.Model):
     choice_text = models.CharField(max_length=256)
     is_correct = models.BooleanField()
 
-class QuizAnswer(models.Model):
-    quiz = models.OneToOneField(Quiz, on_delete=models.CASCADE, related_name='answer_explanation')
-    explanation = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
 class QuizResult(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     attempt_no = models.IntegerField(default=1)
-    selected_answer = models.CharField(max_length=20, help_text="객관식 선택값, OX")
+    selected_answer = models.CharField(max_length=50, help_text="객관식 선택값, OX")
     is_correct = models.BooleanField()
     earned_score = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
