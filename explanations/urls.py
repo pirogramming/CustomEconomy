d32044132936baceb6a46a9e7609da3fec986116
SeungKu-Kang <170948850+SeungKu-Kang@users.noreply.github.com>
2026-02-06 17:15:53 +0900

    feat:ai해석부분 레벨 클릭 시 작동하도록 변경/로딩시간 단축 및 로딩 시 ui추가

--- explanations/urls.py
+++ explanations/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
      path('<int:article_id>/', views.explanation_detail, name='detail'),
+     path('<int:article_id>/generate/<int:level>/', views.generate_level_explanation, name='generate_level'),
 ]
