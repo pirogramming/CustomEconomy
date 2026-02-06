60dc17ff369845051a5c37db880696b0a4cd8a9d
haeuniea <haeunhaa@gmail.com>
2026-02-06 23:19:18 +0900

    퀴즈 오류 해결, 기사 목록 소분류 태깅 보이도록 적용

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('quiz/<int:article_id>/', views.quiz_view, name='quiz'),
+     path('<int:article_id>/', views.quiz_view, name='quiz'),
      path('submit/<int:article_id>/', views.submit_quiz_session, name='submit_quiz_session'),
-     path('wrong-note/', views.my_wrong_note, name='my_wrong_note'),
 ]
