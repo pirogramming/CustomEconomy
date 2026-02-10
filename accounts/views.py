b08d8c1a4a87b40dda2506ad0db121f88fd39df8
lilyyang0077 <lilyyang0077@test.com>
2026-02-10 14:40:11 +0900

    feat: 회원가입 결과창 front  완료

--- accounts/views.py
+++ accounts/views.py
@@ -366,6 +366,7 @@ def result_page(request):
         "total_knowledge": state["total_knowledge"],
         "total": total,
         "level": level,
+        "user_name": request.user.name,
     })
 
 @login_required
